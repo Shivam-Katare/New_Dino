52ff80a221187f692dae387eb161f3e280a98a5c
Shivam Katare <katare27451@gmail.com>
2022-07-04 19:04:54 +0530

    Update all code

---
+++ ground.js
@@ -0,0 +1,23 @@
+import {
+  getCustomProperty,
+  incrementCustomProperty,
+  setCustomProperty,
+} from "./updateCustomProperty.js"
+
+const SPEED = 0.05
+const groundElems = document.querySelectorAll("[data-ground]")
+
+export function setupGround() {
+  setCustomProperty(groundElems[0], "--left", 0)
+  setCustomProperty(groundElems[1], "--left", 18)
+}
+
+export function updateGround(delta, speedScale) {
+  groundElems.forEach(ground => {
+    incrementCustomProperty(ground, "--left", delta * speedScale * SPEED * -1)
+
+    if (getCustomProperty(ground, "--left") <= -18) {
+      incrementCustomProperty(ground, "--left", 36)
+    }
+  })
+}
